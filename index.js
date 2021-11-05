15223cb2852cb9e8b13e8ff8d01c48612a3b4aef
kkyprianou <kkyprianou@dits.dmrid.gov.cy>
2021-11-05 09:31:56 +0200

    Listening Port change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((request, response) => {
     response.end("Hello dsf world!");
 });
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
